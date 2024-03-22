a6e92cdc49ee0eaa6c4e2e6c2870e44fd1c68367
Theoreticallyhugo <me@theoreticallyhugo.com>
2024-03-22 16:22:18 +0100

    general layout of sheet implemented

--- src/ui.rs
+++ src/ui.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 use ratatui::{
     prelude::{Layout, Direction, Rect},
     layout::{Alignment, Constraint},
-    style::{Color, Style},
+    style::{Color, Style, Stylize},
     widgets::{Block, BorderType, Paragraph},
     Frame,
 };
@@ -19,7 +19,7 @@ pub fn render(app: &mut App, frame: &mut Frame) {
 
     let layout = Layout::default()
         .direction(Direction::Vertical)
-        .constraints([Constraint::Length(7), Constraint::Fill(1)])
+        .constraints([Constraint::Length(5), Constraint::Fill(1)])
         .split(frame.size());
 
     first(frame, app, layout[0]);
@@ -28,23 +28,222 @@ pub fn render(app: &mut App, frame: &mut Frame) {
 }
 
 pub fn first(frame: &mut Frame, app: &mut App, area: Rect) {
+    let layout = Layout::horizontal([
+        Constraint::Length(15),
+        Constraint::Length(15),
+        Constraint::Length(15),
+        Constraint::Length(15),
+        Constraint::Length(15),
+        Constraint::Length(15),
+        Constraint::Length(15),
+        Constraint::Length(15),
+        Constraint::Length(15),
+        Constraint::Min(25),
+    ])
+    .split(area);
+
+    first_str(frame, app, layout[0]);
+    first_dex(frame, app, layout[1]);
+    first_con(frame, app, layout[2]);
+    first_int(frame, app, layout[3]);
+    first_wis(frame, app, layout[4]);
+    first_cha(frame, app, layout[5]);
+    first_proficiency(frame, app, layout[6]);
+    first_walking(frame, app, layout[7]);
+    first_inspiration(frame, app, layout[8]);
+    first_hp(frame, app, layout[9]);
+}
+
+pub fn first_str(frame: &mut Frame, app: &mut App, area: Rect) {
     frame.render_widget(
-        Paragraph::new(format!(
-            "This is a tui template.\n\
-                Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
-                Press left and right to increment and decrement the counter respectively.\n\
-                Counter: {}",
-            app.counter
-        ))
+        Paragraph::new(format!("strength\n{}\n{: >2}", "+3", 16))
         .block(
             Block::bordered()
-                .title("T")
+                .title("")
                 .title_alignment(Alignment::Center)
                 .border_type(BorderType::Rounded),
         )
         .style(Style::default().fg(Color::Cyan).bg(Color::Reset))
         .centered(),
         area,
+    );
+}
+
+pub fn first_dex(frame: &mut Frame, app: &mut App, area: Rect) {
+    frame.render_widget(
+        Paragraph::new(format!("dexterity\n{}\n{: >2}", "-1", 8))
+        .block(
+            Block::bordered()
+                .title("")
+                .title_alignment(Alignment::Center)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Cyan).bg(Color::Reset))
+        .centered(),
+        area,
+    );
+}
+
+pub fn first_con(frame: &mut Frame, app: &mut App, area: Rect) {
+    frame.render_widget(
+        Paragraph::new(format!("constitution\n{}\n{: >2}", "+2", 14))
+        .block(
+            Block::bordered()
+                .title("")
+                .title_alignment(Alignment::Center)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Cyan).bg(Color::Reset))
+        .centered(),
+        area,
+    );
+}
+
+pub fn first_int(frame: &mut Frame, app: &mut App, area: Rect) {
+    frame.render_widget(
+        Paragraph::new(format!("intelligence\n{}\n{: >2}", "-1", 8))
+        .block(
+            Block::bordered()
+                .title("")
+                .title_alignment(Alignment::Center)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Cyan).bg(Color::Reset))
+        .centered(),
+        area,
+    );
+}
+
+pub fn first_wis(frame: &mut Frame, app: &mut App, area: Rect) {
+    frame.render_widget(
+        Paragraph::new(format!("wisdom\n{}\n{: >2}", "+1", 12))
+        .block(
+            Block::bordered()
+                .title("")
+                .title_alignment(Alignment::Center)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Cyan).bg(Color::Reset))
+        .centered(),
+        area,
+    );
+}
+
+pub fn first_cha(frame: &mut Frame, app: &mut App, area: Rect) {
+    frame.render_widget(
+        Paragraph::new(format!("charisma\n{}\n{: >2}", "+3", 16))
+        .block(
+            Block::bordered()
+                .title("")
+                .title_alignment(Alignment::Center)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Cyan).bg(Color::Reset))
+        .centered(),
+        area,
+    );
+}
+
+pub fn first_proficiency(frame: &mut Frame, app: &mut App, area: Rect) {
+    frame.render_widget(
+        Paragraph::new(format!("proficiency\n{}\nbonus", "+2"))
+        .block(
+            Block::bordered()
+                .title("")
+                .title_alignment(Alignment::Center)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Cyan).bg(Color::Reset))
+        .centered(),
+        area,
+    );
+}
+
+pub fn first_walking(frame: &mut Frame, app: &mut App, area: Rect) {
+    frame.render_widget(
+        Paragraph::new(format!("walking\n  {: >3} ft.\nspeed", "30"))
+        .block(
+            Block::bordered()
+                .title("")
+                .title_alignment(Alignment::Center)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Cyan).bg(Color::Reset))
+        .centered(),
+        area,
+    );
+}
+
+pub fn first_inspiration(frame: &mut Frame, app: &mut App, area: Rect) {
+    frame.render_widget(
+        Paragraph::new(format!("inspiration\n{}", ""))
+        .block(
+            Block::bordered()
+                .title("")
+                .title_alignment(Alignment::Center)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Cyan).bg(Color::Reset))
+        .centered(),
+        area,
+    );
+}
+
+pub fn first_hp(frame: &mut Frame, app: &mut App, area: Rect) {
+    let layout = Layout::horizontal([
+        Constraint::Percentage(25),
+        Constraint::Percentage(25),
+        Constraint::Percentage(25),
+        Constraint::Percentage(25),
+    ])
+    .split(area);
+    frame.render_widget(
+        Paragraph::new("heal\n\ndamage")
+        .block(
+            Block::bordered()
+                .title("")
+                .title_alignment(Alignment::Center)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Cyan).bg(Color::Reset))
+        .centered(),
+        layout[0],
+    );
+    frame.render_widget(
+        Paragraph::new(format!("current\n{}\nhp", "30"))
+        .block(
+            Block::bordered()
+                .title("")
+                .title_alignment(Alignment::Center)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Cyan).bg(Color::Reset))
+        .centered(),
+        layout[1],
+    );
+    frame.render_widget(
+        Paragraph::new(format!("max\n{}\nhp", "30"))
+        .block(
+            Block::bordered()
+                .title("")
+                .title_alignment(Alignment::Center)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Cyan).bg(Color::Reset))
+        .centered(),
+        layout[2],
+    );
+    frame.render_widget(
+        Paragraph::new(format!("temp\n{}\nhp", "30"))
+        .block(
+            Block::bordered()
+                .title("")
+                .title_alignment(Alignment::Center)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Cyan).bg(Color::Reset))
+        .centered(),
+        layout[3],
     );
 }
 
@@ -64,7 +263,7 @@ pub fn second_left(frame: &mut Frame, app: &mut App, area: Rect) {
     let layout = Layout::vertical([
         Constraint::Min(7),
         Constraint::Min(7),
-        Constraint::Percentage(50),
+        Constraint::Percentage(60),
     ])
     .split(area);
 
@@ -97,9 +296,9 @@ pub fn second_left(frame: &mut Frame, app: &mut App, area: Rect) {
     // SENSES
     frame.render_widget(
         Paragraph::new(
-            format!(" {:0>2} passive wis (perception)\n \
-            {:0>2} passive int (investigation)\n \
-            {:0>2} passive wis (insight)\n\n \
+            format!(" {: >2} passive wis (perception)\n \
+            {: >2} passive int (investigation)\n \
+            {: >2} passive wis (insight)\n\n \
             additional sense types", 
             11, 
             9, 
@@ -135,6 +334,7 @@ pub fn second_left(frame: &mut Frame, app: &mut App, area: Rect) {
         layout[2]
     );
 }
+
 pub fn second_middle(frame: &mut Frame, app: &mut App, area: Rect) {
     frame.render_widget(
         Paragraph::new(
@@ -151,11 +351,109 @@ pub fn second_middle(frame: &mut Frame, app: &mut App, area: Rect) {
         area
     )
 }
+
 pub fn second_right(frame: &mut Frame, app: &mut App, area: Rect) {
+    let layout = Layout::vertical([
+        Constraint::Length(5),
+        Constraint::Percentage(100),
+    ])
+    .split(area);
+    second_right_top(frame, app, layout[0]);
+    second_right_bot(frame, app, layout[1]);
+}
+
+pub fn second_right_top(frame: &mut Frame, app: &mut App, area: Rect) {
+    let layout = Layout::horizontal([
+        Constraint::Percentage(15),
+        Constraint::Percentage(15),
+        Constraint::Percentage(35),
+        Constraint::Percentage(35),
+    ])
+    .split(area);
+    second_right_top_1(frame, app, layout[0]);
+    second_right_top_2(frame, app, layout[1]);
+    second_right_top_3(frame, app, layout[2]);
+    second_right_top_4(frame, app, layout[3]);
+}
+
+pub fn second_right_top_1(frame: &mut Frame, app: &mut App, area: Rect) {
     frame.render_widget(
         Paragraph::new(
-            format!("str: {}", 16)
+            format!("initiative\n{}", "-1")
         ) 
+        .block(
+            Block::bordered()
+                .title("")
+                .title_alignment(Alignment::Center)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Cyan).bg(Color::Reset))
+        .centered(),
+        area
+    )
+}
+
+pub fn second_right_top_2(frame: &mut Frame, app: &mut App, area: Rect) {
+    frame.render_widget(
+        Paragraph::new(
+            format!("armor\n{}\nclass", 19)
+        ) 
+        .block(
+            Block::bordered()
+                .title("")
+                .title_alignment(Alignment::Center)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Cyan).bg(Color::Reset))
+        .centered(),
+        area
+    )
+}
+
+pub fn second_right_top_3(frame: &mut Frame, app: &mut App, area: Rect) {
+    frame.render_widget(
+        Paragraph::new(
+            format!("{} fire\n{} disease", "r", "i")
+        ) 
+        .block(
+            Block::bordered()
+                .title("defenses")
+                .title_alignment(Alignment::Left)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Cyan).bg(Color::Reset))
+        .left_aligned(),
+        area
+    )
+}
+
+pub fn second_right_top_4(frame: &mut Frame, app: &mut App, area: Rect) {
+    frame.render_widget(
+        Paragraph::new(
+            "add active conditions"
+        ) 
+        .block(
+            Block::bordered()
+                .title("conditions")
+                .title_alignment(Alignment::Left)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Cyan).bg(Color::Reset))
+        .left_aligned(),
+        area
+    )
+}
+
+pub fn second_right_bot(frame: &mut Frame, app: &mut App, area: Rect) {
+    frame.render_widget(
+        Paragraph::new(
+            format!(
+            "This is a tui template.\n\
+                Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
+                Press left and right to increment and decrement the counter respectively.\n\
+                Counter: {}",
+            app.counter
+        ))
         .block(
             Block::bordered()
                 .title("right")
